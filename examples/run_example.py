8547301a919b0ccfa45da6f6c05e9d466743c3db
Alexey Stupishin <agstup@yandex.ru>
2024-06-12 23:00:59 +0300

    Preparation

--- examples/run_example.py
+++ examples/run_example.py
@@ -3,30 +3,30 @@
 import os, sys
 import numpy as np
 from mag_field_wrapper import MagFieldWrapper
-from pathlib import Path
 
 #-------------------------------------------------------------------------------
 
 m = sys.modules[__name__]
 this_path = os.path.dirname(m.__file__)
 
-lib_file = this_path + '../src/AMaFiL/binaries/WWNLFFFReconstruction.dll'
+lib_file = this_path + '/../src/AMaFiL/binaries/WWNLFFFReconstruction.dll'
 print("Loading library from", lib_file)
 
 maglib = MagFieldWrapper(lib_file)
 
-data_path = this_path + 'Data/'
-
 # potential
 print('Load potential cube ...')
-maglib.load_cube(data_path / '11312_hmi.M_720s.20111010_085818.W120N23CR.CEA.POT.sav')
+maglib.load_cube(this_path + '/Data/11312_hmi.M_720s.20111010_085818.W120N23CR.CEA.POT.sav')
 energy_pot = maglib.energy
 print('Potential energy: ' + str(energy_pot) + ' erg')
 
 # NLFFF
 print('Load boundary cube ...')
-maglib.load_cube(data_path / '11312_hmi.M_720s.20111010_085818.W120N23CR.CEA.BND.sav')
-### ! NLFFF
+maglib.load_cube(this_path + '/Data/11312_hmi.M_720s.20111010_085818.W120N23CR.CEA.BND.sav')
+print('Calculate NLFFF ...')
+box = maglib.NLFFF()
+energy_nlfff = maglib.energy
+print('NLFFF energy:     ' + str(energy_nlfff) + ' erg')
 
 # Lines (seeds)
 v = maglib.est_max_coords()
@@ -34,7 +34,7 @@ v = maglib.est_max_coords()
 sz = maglib.get_box_size
 input_seeds = np.zeros((np.prod(sz), 3), dtype = np.float64, order="C")
 
-iz = 1
+iz = 0
 porosity = 10
 
 cnt = 0
@@ -49,7 +49,12 @@ print('Prepared ' + str(input_seeds.shape[0]) + ' seeds ...')
 lines = maglib.lines(seeds = input_seeds, max_length = -1)
 print('Passed ' + str(lines['n_passed']) + ', non-passed ' + str(lines['non_passed']))
 
+# Lines (only passed)
 lines = maglib.lines()
+print('Passed ' + str(lines['n_passed']) + ', non-passed ' + str(lines['non_passed']))
+
+# Lines (all)
+lines = maglib.lines(reduce_passed = maglib.PASSED_NONE)
 print('Passed ' + str(lines['n_passed']) + ', non-passed ' + str(lines['non_passed']))
 
 pass
--- src/mag_field_wrapper.py
+++ src/mag_field_wrapper.py
@@ -103,21 +103,23 @@ class MagFieldWrapper:
 
         box = self._as_dict(box[0])
         
-        self.load_cube_vars(box['BY'], box['BX'], box['BZ'], box['DR'][0])
+        self.load_cube_vars(box['BY'], box['BX'], box['BZ'], box['DR'])
+        self.__step *= sun.radius.to(u.cm).value
 
 #-------------------------------------------------------------------------------
-    def load_cube_vars(self, bx, by, bz, dr, dr1 = None, dr2 = None):
+    def load_cube_vars(self, bx, by, bz, dr):
         self.__by = bx.transpose((0, 2, 1)).astype(np.float64, order="C")
         self.__bx = by.transpose((0, 2, 1)).astype(np.float64, order="C")
         self.__bz = bz.transpose((0, 2, 1)).astype(np.float64, order="C")
         Nc = self.__bx.shape
         self.__N = np.array([Nc[2], Nc[1], Nc[0]], dtype = np.int32)
         
-        if dr1 is None:
-            dr1 = dr    
-        if dr2 is None:
-            dr2 = dr    
-        self.__step = (np.array([dr, dr1, dr2], dtype = np.float64)) * sun.radius.to(u.cm).value
+        if np.isscalar(dr):
+            step = [dr, dr, dr]
+        else:
+            step = np.flip(dr)
+            
+        self.__step = (np.array(step, dtype = np.float64))
 
 #-------------------------------------------------------------------------------
     def NLFFF(self
@@ -152,57 +154,75 @@ class MagFieldWrapper:
 
         return totalB2 / 8 / np.pi * volume
 
-    #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+    def est_max_coords(self, n_total = 0):
+        line_length_est = LA.norm(self.__bx.shape)
+        if n_total == 0:
+            n_total = self.__bx.size
+        return np.int64(np.ceil(line_length_est*n_total))
+    
+#-------------------------------------------------------------------------------
     def lines(self
             , reduce_passed = None
             , chromo_level = 1
             , seeds = None
+            , max_length = 0
+            , reshape_3D = True
             , step = 1.0
             , tolerance = 1e-3
             , tolerance_bound = 1e-3
             , n_processes = 0
-            , max_length = None
              ):
 
         # assert box is None
-        line_length_est = LA.norm(self.__bx.shape)
-
         seeds_type = self.__mvoid
         if seeds is None:
             n_seeds = 0
-            seeds = 0
+            arg_seeds = 0
             n_total = self.__bx.size
             if reduce_passed is None:
                 reduce_passed = self.PASSED_CLOSED | self.PASSED_OPENED
         else:
             # assert seeds is not 2D
-            seeds = np.array(seeds, dtype = np.float64, order = 'C')
-            n_seeds = seeds.shape[0]
+            arg_seeds = np.array(seeds, dtype = np.float64, order = 'C')
+            n_seeds = arg_seeds.shape[0]
             seeds_type = self.__mptr2
             n_total = n_seeds
             if reduce_passed is None:
                 reduce_passed = self.PASSED_NONE
 
-        if max_length is None:
-            max_length = np.ceil(line_length_est*n_total).astype(np.int32)
-        # assert max_length*4 too large
+        if max_length < 0:
+            max_length = self.est_max_coords(n_total)
+        max_length = np.int64(max_length)
 
         n_lines = np.array([0], dtype = np.int32)
         n_passed = np.array([0], dtype = np.int32)
         voxel_status = np.zeros([n_total], dtype = np.int32)
         phys_length = np.zeros([n_total], dtype = np.float64)
         av_field = np.zeros([n_total], dtype = np.float64)
-        lines_length = np.zeros([n_total], dtype = np.int32)
         codes = np.zeros([n_total], dtype = np.int32)
         start_idx = np.zeros([n_total], dtype = np.int32)
         end_idx = np.zeros([n_total], dtype = np.int32)
         apex_idx = np.zeros([n_total], dtype = np.int32)
         seed_idx = np.zeros([n_total], dtype = np.int32)
         total_length = np.array([0], dtype = np.uint64)
-        coords = np.zeros([max_length, 4], dtype = np.float64, order="C")
-        lines_start = np.zeros([n_total], dtype = np.uint64)
-        lines_index = np.zeros([n_total], dtype = np.int32)
-
+        
+        if max_length == 0:
+            coords_type = self.__mvoid
+            coords = 0
+            ls_type = self.__mvoid
+            lines_start = 0
+            lv_type = self.__mvoid
+            lines_length = 0
+            lines_index = 0
+        else:
+            coords_type = self.__mptr2
+            coords = np.zeros([max_length, 4], dtype = np.float64, order="C")
+            ls_type = self.__mp64
+            lines_start = np.zeros([n_total], dtype = np.uint64)
+            lv_type = self.__mpint1
+            lines_length = np.zeros([n_total], dtype = np.int32)
+            lines_index = np.zeros([n_total], dtype = np.int32)
 
         lines_func = self.__func_set['lines_func']
         lines_func.argtypes = [self.__mpint1, self.__mptr3, self.__mptr3, self.__mptr3   # 1-4
@@ -211,15 +231,15 @@ class MagFieldWrapper:
                              , self.__mint, self.__mreal, self.__mreal, self.__mreal #   9-12 int nProc = 0, REALTYPE_A step = 1.0, REALTYPE_A tolerance = 1e-3, REALTYPE_A boundAchieve = 1e-3,
                              , self.__mpint1, self.__mpint1                          #   13-14 int *_nLines = nullptr, int *_nPassed = nullptr,
                              , self.__mpint1, self.__mptr1, self.__mptr1             #   15-17 int *_voxelStatus = nullptr, REALTYPE_A *_physLength = nullptr, REALTYPE_A *_avField = nullptr,
-                             , self.__mpint1, self.__mpint1                          #   18-19 int *_linesLength = nullptr, int *_codes = nullptr,
+                             , lv_type, self.__mpint1                                #   18-19 int *_linesLength = nullptr, int *_codes = nullptr,
                              , self.__mpint1, self.__mpint1, self.__mpint1           #   20-22 int *_startIdx = nullptr, int *_endIdx = nullptr, int *_apexIdx = nullptr,
-                             , self.__m64, self.__mp64, self.__mptr2                 #   23-25 uint64_t _maxCoordLength = 0, uint64_t *_totalLength = nullptr, REALTYPE_A *_coords = nullptr, 
-                             , self.__mp64, self.__mpint1, self.__mpint1             #   26-28 uint64_t *_linesStart = nullptr, int *_linesIndex = nullptr, int *seedIdx = nullptr);
+                             , self.__m64, self.__mp64, coords_type                  #   23-25 uint64_t _maxCoordLength = 0, uint64_t *_totalLength = nullptr, REALTYPE_A *_coords = nullptr, 
+                             , ls_type, lv_type, self.__mpint1                       #   26-28 uint64_t *_linesStart = nullptr, int *_linesIndex = nullptr, int *seedIdx = nullptr);
                               ]
 
         non_passed = lines_func(self.__N, self.__bx, self.__by, self.__bz
                       , reduce_passed, chromo_level 
-                      , seeds, n_seeds
+                      , arg_seeds, n_seeds
                       , n_processes, step, tolerance, tolerance_bound
                       , n_lines, n_passed
                       , voxel_status, phys_length, av_field
@@ -228,6 +248,17 @@ class MagFieldWrapper:
                       , max_length, total_length, coords
                       , lines_start, lines_index, seed_idx
                        )
+        
+        if reshape_3D & (seeds is None):
+            av_field = np.reshape(av_field, np.flip(self.__N))
+            phys_length = np.reshape(phys_length, np.flip(self.__N))
+            av_field = np.reshape(av_field, np.flip(self.__N))
+            start_idx = np.reshape(start_idx, np.flip(self.__N))
+            end_idx = np.reshape(end_idx, np.flip(self.__N))
+            seed_idx = np.reshape(seed_idx, np.flip(self.__N))
+            apex_idx = np.reshape(apex_idx, np.flip(self.__N))
+            voxel_status = np.reshape(voxel_status, np.flip(self.__N))
+            codes = np.reshape(codes, np.flip(self.__N))
 
         return dict(n_lines = n_lines[0]
                   , n_passed = n_passed[0]
@@ -247,4 +278,5 @@ class MagFieldWrapper:
                   , lines_index = lines_index
                   , seed_idx = seed_idx
                    )        
-             # reorder - 2do ?
+ 
+       # reorder - 2do ?
